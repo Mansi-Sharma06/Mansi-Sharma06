7e8cd5dd1b55d34b2a5eaba95e8d2d259248c365
Mansi Sharma <78598152+Mansi-Sharma06@users.noreply.github.com>
2025-02-26 19:12:52 +0530

    Update update-shayri.js

--- update-shayri.js
+++ update-shayri.js
@@ -4,22 +4,20 @@ async function updateShayari() {
   try {
     const shayariData = require('./shayris.json');
     const randomIndex = Math.floor(Math.random() * shayariData.length);
-    const selectedShayari = shayariData[randomIndex];  
-
-    const { shayari, author } = selectedShayari;  
+    const { shayari, author } = shayariData[randomIndex];
 
     const cardDesign = `
-<!--STARTS_HERE_QUOTE_CARD-->
+<!--STARTS_HERE_SHAYRI_CARD-->
 <p align="center">
-    <img src="https://readme-daily-quotes.vercel.app/api?author=${encodeURIComponent(author)}&shayari=${encodeURIComponent(shayari)}&theme=dark&bg_color=220a28&author_color=ffeb95&accent_color=c56a90">
+    <img src="https://readme-daily-quotes.vercel.app/api?author=${encodeURIComponent(author)}&quote=${encodeURIComponent(shayari)}&theme=dark&bg_color=220a28&author_color=ffeb95&accent_color=c56a90">
 </p>
-<!--ENDS_HERE_QUOTE_CARD-->
+<!--ENDS_HERE_SHAYRI_CARD-->
 `;
 
     const readmePath = './README.md';
     let readmeContent = fs.readFileSync(readmePath, 'utf-8');
 
-    const regex = /<!--STARTS_HERE_QUOTE_CARD-->[\s\S]*<!--ENDS_HERE_QUOTE_CARD-->/;
+    const regex = /<!--STARTS_HERE_SHAYRI_CARD-->[\s\S]*<!--ENDS_HERE_SHAYRI_CARD-->/;
     readmeContent = readmeContent.replace(regex, cardDesign);
 
     fs.writeFileSync(readmePath, readmeContent);
@@ -30,38 +28,3 @@ async function updateShayari() {
 }
 
 updateShayari();
-
-
-
-
-/* const fs = require('fs');
-
-async function updateShayari() {
-  try {
-    const shayari = require('./shayris.json');
-    const randomIndex = Math.floor(Math.random() * shayari.length);
-    const { shayari, author } = shayari[randomIndex];
-
-    const cardDesign = `
-<!--STARTS_HERE_QUOTE_CARD-->
-<p align="center">
-    <img src="https://readme-daily-quotes.vercel.app/api?author=${encodeURIComponent(author)}&shayari=${encodeURIComponent(quote)}&theme=dark&bg_color=220a28&author_color=ffeb95&accent_color=c56a90">
-</p>
-<!--ENDS_HERE_QUOTE_CARD-->
-`;
-
-    const readmePath = './README.md';
-    let readmeContent = fs.readFileSync(readmePath, 'utf-8');
-
-    readmeContent = readmeContent.replace(
-      /<!--STARTS_HERE_QUOTE_CARD-->(.|\n)*<!--ENDS_HERE_QUOTE_CARD-->/,
-      cardDesign
-    );
-
-    fs.writeFileSync(readmePath, readmeContent);
-  } catch (error) {
-    console.error('Error updating quote:', error);
-  }
-}
-
-updateShayari(); */
